6de75ca7c53a66bd0bec46d5e95f571456f2351c
Karthik <automation.karthik@gmail.com>
2019-07-13 12:30:33 +0530

    Updates

---
+++ precache-manifest.402f6cf1835f5718d8d47ee0c2b4bd9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea52f407d4c831ec99e1f079532823e",
+    "url": "/smart-dashboard/index.html"
+  },
+  {
+    "revision": "1b9f31263da8febb5d93",
+    "url": "/smart-dashboard/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "6072854ba6e14acbd324",
+    "url": "/smart-dashboard/static/css/main.859ced46.chunk.css"
+  },
+  {
+    "revision": "1b9f31263da8febb5d93",
+    "url": "/smart-dashboard/static/js/2.46640373.chunk.js"
+  },
+  {
+    "revision": "6072854ba6e14acbd324",
+    "url": "/smart-dashboard/static/js/main.6ce913f8.chunk.js"
+  },
+  {
+    "revision": "c1851d1665dc50a82d51",
+    "url": "/smart-dashboard/static/js/runtime~main.28fa2132.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/part-monitoring/precache-manifest.973e670919547eac56ea8cbb8e7d08a6.js"
+  "/smart-dashboard/precache-manifest.402f6cf1835f5718d8d47ee0c2b4bd9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/part-monitoring/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smart-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
